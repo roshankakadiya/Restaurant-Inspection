ddff2fd1ca4723de188514733b6d31e783e21583
嘉明宫 <jga104@sfu.ca>
2020-10-31 16:51:53 -0700

    finishing xml and readfile

--- app/src/main/java/com/example/cmpt276group05/MainActivity.java
+++ app/src/main/java/com/example/cmpt276group05/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    //hi
+    }
+
+    public class ReadFile {
+        public void main(String[] args) {
+            try {
+                File myObj = new File("AllViolations.txt");
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
     }
 }
