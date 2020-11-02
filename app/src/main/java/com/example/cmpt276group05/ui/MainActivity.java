f5180a3aca4d762374703973c7446467535f3ae0
bigapple <jga104@sfu.ca>
2020-11-02 10:42:23 -0800

    almost finish xml

--- app/src/main/java/com/example/cmpt276group05/ui/MainActivity.java
+++ app/src/main/java/com/example/cmpt276group05/ui/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.cmpt276group05.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.DynamicLayout;
+import android.text.style.DynamicDrawableSpan;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.example.cmpt276group05.R;
 import com.example.cmpt276group05.model.Inspection;
@@ -26,8 +36,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar1 = getSupportActionBar();
+        actionbar1.setTitle("Surrey Restaurant Inspection");
+
         restaurantManager = RestaurantManager.getInstance(this);
 
         // Testing restaurant manager
@@ -49,6 +66,27 @@ public class MainActivity extends AppCompatActivity {
         Log.d("SIZE", String.valueOf(inspection.size()));
 
         Log.d("numberOfissues", String.valueOf(inspectionManager.numOfIssuesFound("SWOD-AHZUMF")));
+
+        LinearLayout linearL = new LinearLayout(this);
+        ListView listV = new ListView(this);
+
+        final String[] DynamicListElements = new String[] {
+                "Android",
+                "PHP",
+                "Android Studio",
+                "PhpMyAdmin"
+        };
+
+
+
+
+        //create list View
+        ArrayAdapter<String> newarray = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, DynamicListElements);
+        listV.setAdapter(newarray);
+        linearL.addView(listV);
+
+//        this.setContentView(linearL,new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
 
 
     }
