66e690ac13c278559f7aab10f1f39b0f4d9b13ca
yca320 <825919148@qq.com>
2020-11-04 15:34:05 +0800

    add comment #3

--- app/src/main/java/com/example/cmpt276group05/constant/BusinessConstant.java
+++ app/src/main/java/com/example/cmpt276group05/constant/BusinessConstant.java
@@ -1,5 +1,8 @@
 package com.example.cmpt276group05.constant;
 
+/*
+* data from common static string
+* */
 public class BusinessConstant {
     public static final String INSPECTION_DATA="inspection_data";
     public static final String HAZARD_LOW="Low";
